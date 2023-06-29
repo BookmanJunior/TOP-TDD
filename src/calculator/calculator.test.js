e05e7393807b0e028389e7c9316e64ea67e4763b
Azam Gulov <gulovazam@gmail.com>
2023-06-29 16:01:53 +0500

    Add tests

--- src/calculator/calculator.test.js
+++ src/calculator/calculator.test.js
@@ -3,3 +3,15 @@ import calculator from "./calculator.js";
 test("Add", () => {
   expect(calculator.add(2, 2)).toBe(4);
 });
+
+test("Subtract", () => {
+  expect(calculator.subtract(10, 5)).toEqual(5);
+});
+
+test("Multiply", () => {
+  expect(calculator.multiply(5, 3)).toEqual(15);
+});
+
+test("Divide", () => {
+  expect(calculator.divide(20, 2)).toEqual(10);
+});
