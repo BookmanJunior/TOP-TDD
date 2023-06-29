071d4e6426284963b6565507973e5c85d74e1cd4
Azam Gulov <gulovazam@gmail.com>
2023-06-29 19:26:45 +0500

    Add tests

--- src/analyzeArray/analyzeArray.test.js
+++ src/analyzeArray/analyzeArray.test.js
@@ -1,7 +1,25 @@
 import analyzeArray from "./analyzeArray";
 
-test("object assignment", () => {
-  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+const arr = [1, 8, 3, 4, 2, 6];
+
+test("array length", () => {
+  expect(analyzeArray(arr).average).toBe(4);
+});
+
+test("min number", () => {
+  expect(analyzeArray(arr).min).toBe(1);
+});
+
+test("max number", () => {
+  expect(analyzeArray(arr).max).toBe(8);
+});
+
+test("array length", () => {
+  expect(analyzeArray(arr).length).toBe(6);
+});
+
+test("object has all properties", () => {
+  expect(analyzeArray(arr)).toEqual({
     average: 4,
     min: 1,
     max: 8,
